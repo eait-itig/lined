229ae12bfb7b8be8487d3aca1d8e3be7ea868069
David Gwynne <dlg@uq.edu.au>
2021-07-29 08:53:12 +1000

    very basic handling of line parsing.

--- lines.c
+++ lines.c
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
+#include <ctype.h>
 #include <pwd.h>
 #include <netdb.h>
 #include <errno.h>
@@ -19,8 +21,23 @@
 #define LINES_USER	"_lines"
 #define LINES_PORT	"601"
 
+struct conn {
+	struct server			*server;
+	struct event			 ev;
+
+	char				*laddr;
+	char				*lport;
+	char				*raddr;
+	char				*rport;
+
+	char				*buf;
+	size_t				 len;
+	size_t				 off;
+};
+
 struct listener {
 	TAILQ_ENTRY(listener)		 entry;
+	struct server			*server;
 	const char			*host;
 	const char			*port;
 	struct event			 ev;
@@ -37,6 +54,8 @@ static void	server_bind(struct server *s, int,
 static void	server_listen(struct server *s);
 
 static void	listener_accept(int, short, void *);
+
+static void	tcp_read(int, short, void *);
 
 static void	db_connect(struct server *, const char *);
 
@@ -110,6 +129,8 @@ main(int argc, char *argv[])
 	endpwent();
 
 	db_connect(s, conn);
+
+	signal(SIGPIPE, SIG_IGN);
 
 	if (!debug && daemon(1, 1) == -1)
 		err(1, "daemon");
@@ -191,6 +212,7 @@ server_listen(struct server *s)
 	struct listener *l;
 
 	TAILQ_FOREACH(l, &s->listeners, entry) {
+		l->server = s;
 		event_set(&l->ev, EVENT_FD(&l->ev), EV_READ|EV_PERSIST,
 		    listener_accept, l);
 		event_add(&l->ev, NULL);
@@ -200,7 +222,8 @@ server_listen(struct server *s)
 static void
 listener_accept(int fd, short events, void *arg)
 {
-	//struct listener *l = arg;
+	struct listener *l = arg;
+	struct conn *conn;
 	int cfd;
 	struct sockaddr_storage ss;
 	socklen_t sslen = sizeof(ss);
@@ -214,17 +237,177 @@ listener_accept(int fd, short events, void *arg)
 		return;
 	}
 
-	close(cfd);
+	error = getnameinfo((struct sockaddr *)&ss, sslen,
+	    host, sizeof(host), serv, sizeof(serv),
+	    NI_NUMERICHOST|NI_NUMERICSERV);
+	if (error != 0) {
+		lwarnx("connection name lookup: %s", gai_strerror(error));
+		close(cfd);
+		return;
+	}
+
+	conn = malloc(sizeof(*conn));
+	if (conn == NULL) {
+		lwarn("%s port %s connection", host, serv);
+		goto close;
+	}
+
+	conn->raddr = strdup(host);
+	if (conn->raddr == NULL) {
+		lwarn("%s port %s connection raddr", host, serv);
+		goto free_conn;
+	}
+
+	conn->rport = strdup(serv);
+	if (conn->rport == NULL) {
+		lwarn("%s port %s connection rport", host, serv);
+		goto free_raddr;
+	}
+
+	sslen = sizeof(ss);
+	if (getsockname(cfd, (struct sockaddr *)&ss, &sslen) == -1)
+		lerr(1, "connection getsockname");
 
 	error = getnameinfo((struct sockaddr *)&ss, sslen,
 	    host, sizeof(host), serv, sizeof(serv),
 	    NI_NUMERICHOST|NI_NUMERICSERV);
 	if (error != 0) {
-		lwarnx("accept name: %s", gai_strerror(error));
-		return;
+		lwarnx("connection local name lookup: %s",
+		    gai_strerror(error));
+		goto free_rport;
 	}
 
-	linfo("connection from %s port %s", host, serv);
+	conn->laddr = strdup(host);
+	if (conn->laddr == NULL) {
+		lwarn("%s port %s connection laddr", host, serv);
+		goto free_rport;
+	}
+
+	conn->lport = strdup(serv);
+	if (conn->lport == NULL) {
+		lwarn("%s port %s connection lport", host, serv);
+		goto free_laddr;
+	}
+
+	conn->buf = malloc(1024);
+	if (conn->buf == NULL) {
+		lwarn("%s port %s connection buffer", host, serv);
+		goto free_lport;
+	}
+	conn->len = 1024;
+	conn->off = 0;
+
+	conn->server = l->server;
+
+	event_set(&conn->ev, cfd, EV_READ|EV_PERSIST, tcp_read, conn);
+	event_add(&conn->ev, NULL);
+
+	ldebug("connection from %s", conn->raddr);
+
+	return;
+
+free_lport:
+	free(conn->lport);
+free_laddr:
+	free(conn->laddr);
+free_rport:
+	free(conn->rport);
+free_raddr:
+	free(conn->raddr);
+free_conn:
+	free(conn);
+close:
+	close(cfd);
+}
+
+
+static void
+conn_close(struct conn *conn)
+{
+	event_del(&conn->ev);
+	close(EVENT_FD(&conn->ev));
+
+	free(conn->buf);
+	free(conn->lport);
+	free(conn->laddr);
+	free(conn->rport);
+	free(conn->raddr);
+	free(conn);
+}
+
+static void
+tcp_read(int cfd, short events, void *arg)
+{
+	struct conn *conn = arg;
+	char *head, *tail, *cur;
+	ssize_t rv;
+	size_t len, i;
+
+	head = conn->buf;
+	tail = head + conn->off;
+
+	rv = read(cfd, tail, conn->len - conn->off);
+	switch (rv) {
+	case -1:
+		switch (errno) {
+		case EINTR:
+		case EAGAIN:
+			/* try again later */
+			break;
+		default:
+			lwarn("%s read", conn->raddr);
+			break;
+		}
+		return;
+	case 0:
+		ldebug("closing connection from %s", conn->raddr);
+		conn_close(conn);
+		return;
+	default:
+		break;
+	}
+
+	len = rv;
+	for (i = 0; i < len; i++) {
+		cur = &tail[i];
+		int ch = *cur;
+
+		if (ch == '\n' || ch == '\0') {
+			/* terminate string and trim trailing whitespace */
+			for (;;) {
+				*cur = '\0';
+				if (cur == head)
+					break;
+				cur--;
+				if (!isspace(*cur))
+					break;
+				*cur = '\0';
+			}
+			/* trim leading whitespace */
+			while (head < cur) {
+				if (!isspace(*head))
+					break;
+				head++;
+			}
+
+			if (cur == head)
+				ldebug("empty line from %s", conn->raddr);
+			else
+				linfo("line from %s: %s", conn->raddr, head);
+
+			head = tail + i + 1;
+			continue;
+		}
+	}
+
+	cur = tail + len;
+	if (head == cur) {
+		/* all the input has been consumed */
+		conn->off = 0;
+	} else if (head != conn->buf) {
+		linfo("buf %p head %p tail %p", conn->buf, head, tail);
+		conn->off = 0;
+	}
 }
 
 static void
